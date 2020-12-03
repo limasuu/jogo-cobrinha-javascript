ee11191baf7c4006b09e37da8156be43b5ad8b2d
limasuu <szlima.93@gmail.com>
2020-12-03 19:03:09 -0200

    Criar cobrinha.

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas= document.getElementById("cobrinha");
 let context= canvas.getContext("2d");
 let box= 32;
+let cobrinha= [];
+cobrinha[0]={
+	x: 8*box,
+	y: 8*box
+}
 
 function criarBackground(){
-	context.fillStyle= "lightgreen";
+	context.fillStyle= "peachpuff";
 	context.fillRect(0, 0, 16*box, 16*box);
 }
 
+function criarCobrinha(){
+	for(i=0; i<cobrinha.length; i++){
+		context.fillStyle= "mediumvioletred";
+		context.fillRect(cobrinha[i].x, cobrinha[i].y, box, box);
+	}
+}
+
 criarBackground();
+criarCobrinha();
